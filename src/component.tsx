5c752089d585d7b3477a57b35317b244a64b8d9c
Eszter Hofmann <hofmann@textkernel.nl>
2021-11-26 15:00:09 +0100

    update library, add other example

--- src/component.tsx
+++ src/component.tsx
@@ -1,11 +1,14 @@
-import {Trans, useTranslation} from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 const Component = () => {
-  const {t} = useTranslation()
-  return <div>
-      <Trans>This should be only the key, not the value</Trans>
-      {t('This is just the key, as it should be')}
-    </div>
-}
+    const { t } = useTranslation();
+    return (
+        <div>
+            <Trans i18nKey="trans1">This should be only the key trans1, not the value</Trans>
+            <Trans i18nKey="trans2">This should be only the key trans2, <em>not the value</em></Trans>
+            {t('This is just the key, as it should be')}
+        </div>
+    );
+};
 
-export default Component
+export default Component;
